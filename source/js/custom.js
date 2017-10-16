cd13f20b239d01c7583a03a09b3f491c8b4bc36d
longht1989 <longht@epi.com.vn>
2017-10-16 16:34:36 +0700

    Update basic page Báo Nghệ An mobile
    first beta

--- source/js/custom.js
+++ source/js/custom.js
@@ -8,7 +8,7 @@ $(function() {
         $(this).toggleClass('is-active');
         $(".mobile__search").slideToggle();
         $('.site-header__menu').removeClass('is-active');
-		$(".mobile__menu").slideUp('fast');
+        $(".mobile__menu").slideUp('fast');
     });
     /*show mobile menu*/
     $('.site-header__menu').click(function() {
@@ -17,6 +17,20 @@ $(function() {
         $('.site-header__search').removeClass('is-active');
         $(".mobile__search").slideUp('fast');
     });
+    /*show quickview panel*/
+    $('.sprite-flash').click(function() {
+        $('.quickview').addClass('is-active');
+        $('.quickview-wrap').addClass('is-active');
+    });
+    $('.quickview-mask').click(function() {
+        $('.quickview-wrap').removeClass('is-active');
+        $('.quickview').removeClass('is-active');
+    });
     // define story with gif thumbnail
     $('.story__thumb img[src$=".gif"]').parents('.story').addClass('story--gif');
+    // scroll to top 
+    $(".sprite-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 });
